e7a92551e2adf2a767987223fb573e51a059c9ee
Ivan.Orlov <ivan@morrr.com>
2017-04-20 09:29:37 +0200

    Ignore

--- .fusebox/cache/2.0.0-beta.1/F6gbH-app/static/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = { contents: "\"use strict\";\nexports.__esModule = true;\nvar fs = require(\"fs\");\nvar path = require(\"path\");\nrequire(\"./foo.scss\");\nrequire(\"./bar.scss\");\nvar myPackage = fs.readFileSync(path.resolve(\"./package.json\")).toString();\nvar test = document.querySelector(\"#test\");\ntest.innerHTML = myPackage;\n",
-dependencies: ["fs","path","./foo.scss","./bar.scss"],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1492673117000
-};
