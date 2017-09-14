41af42c2ca969a086d2e2486f34e77c78463016e
Grzegorz Klimek <kfiku.com@gmail.com>
2017-09-14 18:27:37 +0200

    comment out autoupdate for now

--- electron.js
+++ electron.js
@@ -1,37 +1,30 @@
 const env = process.env.NODE_ENV || 'production';
 const dev = env === 'development';
 
-const electron = require('electron')
+const isDevElectron = require('electron-is-dev');
+    // is dev electron (run from builderd)
+
+const autoUpdater = require('./src/mainProcess/autoUpdater')
+const electron = require('electron');
     // Module to control application life.
 
-const app = electron.app
+const app = electron.app;
     // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
+const version = app.getVersion();
 
-const path = require('path')
-const url = require('url')
-const log = require('electron-log');
-const {autoUpdater} = require("electron-updater");
-const {dialog} = require('electron')
-
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function sendStatusToWindow(text) {
-    dialog.showMessageBox({ message: text })
-    console.log(text);
-    log.info(text);
-    mainWindow.webContents.send('message', text);
-}
-
 function createWindow() {
-
-    autoUpdater.checkForUpdates();
+    if (!isDevElectron) {
+        // autoUpdater.checkForUpdates();
+    }
 
     // Create the browser window.
     mainWindow = new BrowserWindow({
@@ -41,23 +34,21 @@ function createWindow() {
     })
     // console.log(app.getVersion());
     if(dev) {
-        mainWindow.loadURL('http://localhost:4444')
+        mainWindow.loadURL(`http://localhost:4444?version=${version}`);
         // and load the index.html of the app.
     
         // Open the DevTools.
-        mainWindow.webContents.openDevTools()
+        mainWindow.webContents.openDevTools();
     } else {
         mainWindow.loadURL(url.format({
-            pathname: path.join(__dirname, 'dist/static/index.html'),
+            // pathname: path.join(__dirname, 'static', `index.html?version=${version}`),
+            // pathname: path.join(__dirname, 'dist', 'static', `index.html?version=${version}`),
+            pathname: path.join(__dirname, 'dist', 'static', `index.html`),
             protocol: 'file:',
             slashes: true
-        }))
-        mainWindow.webContents.openDevTools()
+        }));
+        // mainWindow.webContents.openDevTools();
     }
-
-    setTimeout(() => {
-        sendStatusToWindow('testing messages after 1000')
-    }, 1000)
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
@@ -65,22 +56,22 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
-    })
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    // if (process.platform !== 'darwin') {
         app.quit()
-    }
-})
+    // }
+});
 
 app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
@@ -88,40 +79,7 @@ app.on('activate', function() {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
-autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater.');
-    console.log(err);
-    dialog.showMessageBox({ message: err.toString() })
-})
-autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded; will install in 5 seconds');
-});
-autoUpdater.on('update-downloaded', (info) => {
-    // Wait 5 seconds, then quit and install
-    // In your application, you don't need to wait 5 seconds.
-    // You could call autoUpdater.quitAndInstall(); immediately
-    setTimeout(function() {
-      autoUpdater.quitAndInstall();  
-    }, 5000)
-})
